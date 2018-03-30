d568e36ece41859226ebf331ce9fb3d56e4e74a2
forgetful <oksana14888@.com>
2018-03-30 21:32:18 +0500

    1

---
+++ app/script/script.js
@@ -0,0 +1,39 @@
+//		$(document).ready(function(){
+//			var touch = $('#touch-menu');
+//		    var menu = $('.main-menu');
+//		 
+//		    $(touch).on('click', function(e) {
+//		        e.preventDefault();
+//		        menu.slideToggle();
+//		    });
+//		    $(window).resize(function(){
+//		        var wid = $(window).width();
+//		        if(wid > 760 && menu.is(':hidden')) {
+//		            menu.removeAttr('style');
+//		        }
+//		    });
+//		});
+
+//$(".cont-logo").click(function(){
+//    $(".span").eq(1).toggleClass("span1");
+//})
+
+//alert('11122233');
+//		$(document).ready(function(){
+//			var touch = $('#touch-menu');
+//		    var menu = $('.main-menu');
+//		 
+//		    $(touch).on('click', function(e) {
+//		        e.preventDefault();
+//		        menu.slideToggle();
+//		    });
+//		    $(window).resize(function(){
+//		        var wid = $(window).width();
+//		        if(wid > 760 && menu.is(':hidden')) {
+//		            menu.removeAttr('style');
+//		        }
+//		    });
+//		});
+
+$(".main-a").css("color" , "red");
+$('body').css('background-color', 'red');
